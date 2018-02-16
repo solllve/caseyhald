3854862aaf8df1ca105c74e32cf5f20a26e2a3d5
caseyhald <privateer.acc@gmail.com>
2018-02-15 19:49:26 -0800

    website commit

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+// Variables
+var $links = $('.menu li a');
+var $portfolioToggle = $('[data-toggle="portfolio"]');
+var $portfolioClose = $('[data-toggle="portfolio--close"]');
+var $portfolio = $('.work');
+var $body = $('body');
+var $logo = $('.logo');
+var $gif = $('[data-src="gif"]');
+var $imgThumb = $('.img--thumb').not('.--full');
+var $modalFull = $('.img--thumb.--full');
+var $workBody = $('.work');
+
+$logo.fadeIn(2000);
+
+$portfolioToggle.mouseover(function(){
+  $body.addClass('gradient-bg');
+});
+
+$logo.click(function(){
+  $portfolio.slideDown();
+  $portfolio.find('.work__container').delay(500).fadeIn(1000);
+});
+
+$portfolioToggle.click(function(){
+  $portfolio.slideDown();
+  $portfolio.find('.work__container').delay(500).fadeIn(1000);
+});
+
+$portfolioClose.click(function(){
+  $portfolio.slideUp();
+  $portfolio.find('.work__container').hide();
+});
+
+$links.mouseout(function(){
+  $body.removeClass('gradient-bg');
+});
+
+$(window).scroll(function() {
+   var hT = $('[data-toggle="load_gifs"]').offset().top,
+       hH = $('[data-toggle="load_gifs"]').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH)){
+       $gif.fadeIn(1000);
+   }
+   else {
+     $gif.hide();
+   }
+});
+
+$imgThumb.mouseover(function(e){
+  var $this = $(this);
+  $this.addClass('--floating');
+});
+
+$imgThumb.mouseout(function(e){
+  var $this = $(this);
+  $this.removeClass('--floating');
+});
+
+$imgThumb.click(function(e){
+  var $this = $(this);
+  var $imgURL = $this.find('img').attr('src');
+  var $modal = $('.img--thumb.--full');
+  $modal.removeClass('--hide');
+  $modal.html('<img src="'+ $imgURL +'" />');
+  $workBody.addClass('blur');
+});
+
+$modalFull.click(function(e){
+  var $this = $(this);
+  $this.addClass('--hide');
+  $this.html('');
+  $workBody.removeClass('blur');
+});
+
+//if ($(window).width() < 960) {
+//   alert('Less than 960');
+//}
+//else {
+//   alert('More than 960');
+//}
